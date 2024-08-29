6eeb8c4c8d64d870b9c1c773695825a46be7eb53
haroldskie <47178309+haroldskie@users.noreply.github.com>
2024-08-29 12:28:06 +0000

    finish calcu

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+// addition - Victor
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5,10));
 
 
 
+// subtraction - Kaira
+let a = 5; 
+let b = 4; 
+  
+function subtract(a, b) { 
+    return a - b; 
+} 
+  
+console.log("total : " 
+    + subtract(a, b));
 
+function subtraction(a, b) {
+        let diff = a - b;
+        return diff;
+    }
+    console.log(diff(1, 2));
 
-
-
-
-
-
-
-
-// multiplication
+// multiplication - Harold
 
 function multiply(a, b) {
     let product = a * b;
